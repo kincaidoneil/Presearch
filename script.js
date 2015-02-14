d5d734426cdb248f0ce1e3003213af8d1aa1fbc2
Kincaid O'Neil <kincaidoneil@gmail.com>
2015-02-14 13:34:04 -0600

    Initial commit :D

---
+++ script.js
@@ -0,0 +1,30 @@
+sampleData = {hello: 3, foo: "bar"};
+
+saveLinks(sampleData, function() {
+	loadLinks(function(data) {
+		console.log(data);
+	});
+});
+
+function loadLinks(callback) {
+	
+	chrome.storage.sync.get('bookmarkData', function(data) {
+		// Check if no links exist
+		if ($.isEmptyObject(data)) {
+			// Error!
+			return false;
+		} else {
+			// Success!
+			callback(data);
+		}
+	});
+	
+}
+
+function saveLinks(data, callback) {
+	
+	chrome.storage.sync.set({'bookmarkData': data}, function(data) {
+		callback();
+	});
+	
+}
