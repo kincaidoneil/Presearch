c8167358573766aec7b00484c0331c57202beb13
Kincaid O'Neil <kincaidoneil@gmail.com>
2015-02-14 21:45:51 -0600

    Add other small changes

--- inject/js/tooltip.js
+++ inject/js/tooltip.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
 	
-	console.log('jQuery loaded in the injected code.');
-	
-	$('body').css('position', 'relative');
-	
 	$('a').tooltipster({
 		arrow: true,
 		content: $('<div style="width: 200px; height: 300px">Hi! What is up?<\/div>'),
@@ -11,11 +7,9 @@ $(document).ready(function() {
 		delay: 0,
 		position: 'right',
 		interactive: true,
-		functionInit: function(origin, continueTooltip) {
-			console.log(
-				$(origin).attr('href')
-			);
-			continueTooltip();
+		functionInit: function(origin, content) {
+			// $(origin).attr('href')
+			// return content;
 		}
 	});
 	
--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-sampleData = {hello: 3, foo: "bar"};
-
-saveLinks(sampleData, function() {
-	loadLinks(function(data) {
-		console.log(data);
-	});
-});
-
 function loadLinks(callback) {
 	
 	chrome.storage.sync.get('bookmarkData', function(data) {
@@ -30,17 +22,5 @@ function saveLinks(data, callback) {
 }
 
 $(document).ready(function() {
-	
-	// $("#declaration").data("powertip", "This will be the <b>tooltip text</b>.");
-	// $("#declaration").powerTip({placement: "e", mouseOnToPopup: true});
-	
-	// $('#declaration').tooltipster({
-	// 	content: $('<h5>Hi! What is up?<\/h5>')
-	// });
-	
-	// chrome.tabs.executeScript(null, {file: "scripts/jquery.min.js"});
-	// chrome.tabs.executeScript(null, {file: "scripts/jquery.powertip.min.js"});
-	// chrome.tabs.executeScript(null, {file: "scripts/jquery.tooltipster.min.js"});
-	// chrome.tabs.executeScript(null, {file: "scripts/tooltip.js"});
-	
+	console.log(chrome.extension.getURL("images/myimage.png"));
 });
